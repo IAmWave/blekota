f824042615538bd81dc3d87ce9826ae0afa50256
Václav <vaclav.volhejn@gmail.com>
2017-01-17 19:13:29 +0100

    Moved gradient check to gru_layer.py

--- src/gru_layer.py
+++ src/gru_layer.py
@@ -61,3 +61,45 @@ class GRULayer:
                 dh[t - 1] += dh[t] * (1 - z[t]) + dq * r[t] + dxh[self.x_n:-1]
 
         self.dW, self.dWr, self.dWz = dW, dWr, dWz
+
+
+# based on https://gist.github.com/karpathy/d4dee566867f8291f086#gistcomment-1508982
+def gradCheck(l=GRULayer(1, 10)):
+
+    def cost(h):
+        dh = h - np.linspace(-1, 1, h.shape[0])[:, None]
+        return 0.5 * np.sum(dh * dh)
+
+    num_checks, delta = 5, 1e-5
+    n = 20
+    x = np.arange(n)[:, None]
+
+    h = l.forward(x)
+    dh = h - np.linspace(-1, 1, n)[:, None]
+
+    l.backward(dh)
+
+    for param, dparam, name in zip([l.W, l.Wr, l.Wz],
+                                   [l.dW, l.dWr, l.dWz],
+                                   ['W', 'Wr', 'Wz']):
+        s0 = dparam.shape
+        s1 = param.shape
+        assert s0 == s1, ('Error dims dont match: %s and %s.' % repr(s0), repr(s1))
+        print(name)
+
+        for i in range(num_checks):
+            ri = int(np.random.randint(param.size))
+            # evaluate cost at [x + delta] and [x - delta]
+            old_val = param.flat[ri]
+            param.flat[ri] = old_val + delta
+            cg0 = cost(l.forward(x))
+            param.flat[ri] = old_val - delta
+            cg1 = cost(l.forward(x))
+            param.flat[ri] = old_val  # reset old value for this parameter
+            # fetch both numerical and analytic gradient
+            grad_analytic = dparam.flat[ri]
+            grad_numerical = (cg0 - cg1) / (2 * delta)
+
+            rel_error = abs(grad_analytic - grad_numerical) / abs(grad_numerical + grad_analytic)
+            print('%f, %f => %e ' % (grad_numerical, grad_analytic, rel_error))
+            # rel_error should be on order of 1e-7 or less
