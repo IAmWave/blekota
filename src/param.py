ac4687f1179d2a02505e4d5fe6f4480aa27baaec
Václav <vaclav.volhejn@gmail.com>
2017-01-29 15:24:42 +0100

    RMSProp instead of Adagrad

--- src/param.py
+++ src/param.py
@@ -3,15 +3,17 @@ import numpy as np
 
 class Param:
 
+    decay = 0.95
+
     def __init__(self, a, name=''):
         self.a = a
         self.d = np.zeros_like(a)
         self.m = np.zeros_like(a)
         self.name = name
 
-    # adagrad update
     def step(self, alpha):
         np.clip(self.d, -5, 5, out=self.d)
-        self.m += self.d ** 2
+        # self.m += self.d ** 2 # adagrad
+        self.m = self.decay * self.m + (1 - self.decay) * (self.d ** 2)  # rmsprop
         self.a += -alpha * self.d / np.sqrt(self.m + 1e-8)
         self.d = np.zeros_like(self.a)
